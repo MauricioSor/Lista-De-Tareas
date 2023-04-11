6768afd4d37be53ba18eff476da81b15fa185110
Mauricio <mauricioutn2017@gmail.com>
2023-04-11 14:16:04 -0300

    Agregado animacion de fondo

--- js/app.js
+++ js/app.js
@@ -7,20 +7,18 @@ botonCrear.addEventListener("click",agregar);
 
 function agregar() {
     let nuevoDiv = document.createElement("div");
-    let texto= document.createElement("p");
+    nuevoDiv.style.display="flex";
+    nuevoDiv.style.margin="10px";
+    let texto= document.createElement("h5");
     let borrar = document.createElement(`button`);
+    borrar.style.backgroundColor=`blue`;
+    borrar.style.borderRadius=`8px`;
     borrar.innerHTML=`Borrar`;
     texto.innerHTML=inputTarea.value;
     nuevoDiv.appendChild(borrar);
     nuevoDiv.appendChild(texto); //añade texto al div creado.
     document.body.appendChild(nuevoDiv);
-
     borrar.addEventListener ("click",function() {
     nuevoDiv.remove();
 });
 }
-/*botonBorrar = addEventListener("click", borrar);
-function borrar() {
-    botonBorrar.style.display = `none`;
-    tareaNueva.style.display = `none`;
-}*/
