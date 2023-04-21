0eaac3a6d358a535cb7239b4de2d88cf16ac8950
Mauricio <mauricioutn2017@gmail.com>
2023-04-21 01:33:49 -0300

    Solucionado bug al agregar tarea

--- js/app.js
+++ js/app.js
@@ -6,18 +6,23 @@ botonCrear.addEventListener("click",agregar);
 // Funciones
 
 function agregar() {
-    let nuevoDiv = document.createElement("div");
+    let contenedor = document.querySelector(`main`);
+    let nuevoDiv = document.getElementById("tareas"); //Leo div de html
+    let texto= document.createElement(`h5`); //Creo un h5
+    let borrar = document.createElement(`button`); // Creo un boton
+    // Estilo de Div
     nuevoDiv.style.display="flex";
     nuevoDiv.style.margin="10px";
-    let texto= document.createElement("h5");
-    let borrar = document.createElement(`button`);
-    borrar.style.backgroundColor=`blue`;
+    //Estilo de boton borrar
+    borrar.style.backgroundColor=`red`;
     borrar.style.borderRadius=`8px`;
-    borrar.innerHTML=`Borrar`;
-    texto.innerHTML=inputTarea.value;
+    borrar.style.marginLeft=`5px`
+    texto.innerHTML= inputTarea.value; //Busco valores de el input
+    borrar.innerHTML=`Borrar`; 
+    contenedor.appendChild(nuevoDiv);
+    //document.querySelector("main").appendChild(nuevoDiv);
+    nuevoDiv.appendChild(texto); //Añade texto al div creado.
     nuevoDiv.appendChild(borrar);
-    nuevoDiv.appendChild(texto); //añade texto al div creado.
-    document.body.appendChild(nuevoDiv);
     borrar.addEventListener ("click",function() {
     nuevoDiv.remove();
 });
